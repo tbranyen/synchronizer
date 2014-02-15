0179a1f679de66312388c879a4997de1cbbc8c88
Mike Pennisi <mike@mikepennisi.com>
2014-02-14 22:29:47 -0500

    Update failing tests

--- test/optimize.js
+++ test/optimize.js
@@ -1,7 +1,12 @@
 var optimize = require('../').optimize;
 
 suite('optimize', function() {
-  test('optimize twice', function() {
+  test('optimize `require` call', function() {
+    optimize('test/fixtures/a.js');
+    optimize('test/fixtures/b.js');
+  });
+
+  test('optimize `define` call twice', function() {
     optimize('test/fixtures/b.js');
     optimize('test/fixtures/b.js');
   });
